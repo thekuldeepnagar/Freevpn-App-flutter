17da17f787ca289a40cafc5264fce75c662714e7
Kuldeep <nagarkuldeep985@gmail.com>
2024-05-08 11:36:26 +0530

    Change App bar name

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'OpenVpn Demo',
+      title: 'Open Demo',
       home: SplashScreen(),
 
       //theme
